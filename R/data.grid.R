31ec362407f5300242fbb8d1f83454c33a639fa9
rubenfcasal <rubenfcasal@gmail.com>
2017-10-18 13:39:19 +0200

    `as.data.frame.data.grid`

--- R/data.grid.R
+++ R/data.grid.R
@@ -7,15 +7,14 @@
 #       dimnames.data.grid(x)
 #       dim.data.grid(x)
 #   interp.data.grid()
+#   as.data.frame.data.grid(x, data.ind = NULL, coords = FALSE, sp = FALSE, 
+#                           row.names = NULL, check.names = coords,  ...) 
 #
 # PENDENTE:
 #   - exemplos
 #   - as.data.grid()
-#   - as.data.frame.data.grid()
-#       !sapply(data.grid, is.list) & (sapply(data.grid, length) == prod(dim(data.grid)))
-#       coords = TRUE, ns <- names(coords) if(any(ns %in% names) ns <- paste("coord", ns, sep=".")
 #
-#   (c) R. Fernandez-Casal         Last revision: Aug 2012
+#   (c) R. Fernandez-Casal         Last revision: Aug 2017
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -71,3 +70,50 @@ data.grid <- function(..., grid = NULL) {
 } # data.grid
 
 
+#' @rdname data.grid
+#' @method as.data.frame data.grid
+#' @param x a \code{data.grid} object.
+#' @param data.ind integer or character vector with the indexes or names of the components.
+#' @param coords logical; if \code{TRUE}, the (spatial) coordinates of the object are added. 
+#' @param sp logical; if \code{TRUE}, the second dimension of the data is reversed 
+#' (as it is stored in \pkg{sp} package). 
+#' @param row.names \code{NULL}, column to be used as row names, or vector giving the row names for the data frame.
+#' @param check.names logical; if \code{TRUE}, the names of the variables in the data 
+#' frame are checked and adjusted if necessary.
+#       coords = TRUE, ns <- names(coords) if(any(ns %in% names) ns <- paste("coord", ns, sep=".")
+as.data.frame.data.grid <- function(x, data.ind = NULL, coords = FALSE, sp = FALSE, row.names = NULL, check.names = coords,  ...){
+  # A botch...
+  index <- !sapply(x, is.list) & (sapply(x, length) == prod(dim(x)))
+  # verificar dimensiones...
+  if (is.null(data.ind)) {
+    data.ind <- which(index)
+  } else {
+    if (!all(index[data.ind], na.rm = TRUE))  stop("Invalid argument 'data.ind'")
+  }
+  res <- x[data.ind]
+  if(sp && (length(dim(x)) > 1)) 
+    res <- lapply(res, function(d) revdim(array(d, dim = dim(x)),2))
+  res <- lapply(res, as.vector)
+  
+  if (coords) 
+    res <- data.frame(coords(x), res, row.names = row.names, check.names = check.names)
+  else  
+    res <- data.frame(res, row.names = row.names)
+  return(res)        
+}
+
+
+# [R] Reversing one dimension of an array, in a generalized case
+# https://stat.ethz.ch/pipermail/r-help/2017-June/thread.html#447298
+# Jeff Newmiller jdnewmil at dcn.davis.ca.us 
+#' @rdname npsp-internals
+#' @keywords internal
+revdim <- function(a, d) {
+  dims <- attr(a, "dim")
+  idxs <- lapply(seq_along(dims),
+                 function(dd) {
+                   if (d == dd) seq.int(dims[dd], 1, -1)
+                   else seq.int(dims[dd])
+                 })
+  do.call(`[`, c(list(a), idxs))
+}
